a1f95eb4973068e38f521d08ae87a1002cf5ba23
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-08-04 12:32:40 +0500

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Thor on 04/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
